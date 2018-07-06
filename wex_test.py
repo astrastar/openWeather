676c1071828dc4b2acd85315b844fafd1b1e149c
astrastar <37809311+astrastar@users.noreply.github.com>
2018-07-06 06:31:39 -0400

    Update wex_test.py

--- wex_test.py
+++ wex_test.py
@@ -66,3 +66,67 @@ while num < 3:
     i += 1
 print(list_href)
 
+#############################################################################################################
+import requests
+from bs4 import BeautifulSoup
+import time
+
+# req = 'тренажерные%20залы'
+# num = 2
+# page = requests.get(f'https://moscow.flamp.ru/search/тренажерные%20залы?page={num}')
+# soup = BeautifulSoup(page.text, 'html5lib')
+# links = soup.find_all(class_='card__link')
+# href_list = []
+
+
+def one_page_parse(links_list):
+    for l in range(len(links_list)):
+        lnk = links_list[l].get('href')
+        # href_list.append(lnk)
+        with open('fit_links', 'a') as fl:
+            fl.writelines(lnk + '\n')
+
+
+def main_parse(pages_qty):
+    """
+    :param pages_qty: количество страниц для парсинга
+    :return:
+    """
+    q = 1
+    while q <= pages_qty:
+        page = requests.get(f'https://moscow.flamp.ru/search/тренажерные%20залы?page={q}')
+        soup = BeautifulSoup(page.text, 'html5lib')
+        links = soup.find_all(class_='card__link')
+        one_page_parse(links)
+        q += 1
+        time.sleep(1)
+
+
+main_parse(81)
+
+# list_href = []
+# while num < 3:
+#     page = requests.get(f'https://moscow.flamp.ru/search/{req}?page={num}')
+#     soup2 = BeautifulSoup(page.text, 'html5lib')
+#     links = soup2.find_all('a', class_='card__link')
+#     for i in range(len(links)):
+#         list_href.append(links[i].get('href'))
+#     i += 1
+# print(list_href)
+
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+req = requests.get('https://moscow.flamp.ru/firm/zebra_fitness_sportivno_ozdorovitelnyjj_kompleks-4504127908530212')
+soup = BeautifulSoup(req.text, 'html5lib')
+feed = soup.find_all(class_='t-rich-text__p')
+# text = feed.contents[0]
+# text1 = str(re.findall(r'\S+', text))
+print(feed)
+# with open('feed', 'a') as f:
+#     f.write(text)
+
+
+
